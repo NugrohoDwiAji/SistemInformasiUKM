5bbb498746ba7dae1a3d2dddc4c0cc2a3e691d62
NugrohoDwiAji <nugrohodwiaji@gmail.com>
2024-05-14 11:51:09 +0800

    memperbaiki sistem file

--- frontend/src/page/AboutPage.jsx
+++ frontend/src/page/AboutPage.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
   return (
     <div className="bg-primary min-h-screen">
       <h2 className="text-center text-black text-2xl font-bold py-4 font-sans">Daftar UKM</h2>
-      <DaftarUKM />
+      <DaftarUkm />
     </div>
   );
 }
